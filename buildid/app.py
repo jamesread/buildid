0e12e31f4a004c0d547245c0ad7b6ab9db8c47e8
jamesread <contact@jwread.com>
2016-02-15 13:17:18 +0000

    Fixed reference to handler

--- buildid/app.py
+++ buildid/app.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
 		outputHandler.write(buildProperties(version));
 
 		import os
-		printInfo("Wrote file: " + os.getcwd() + "/" + handler.getFilename() + ". View the file or just run `buildid` again to see all the properties.");
+		printInfo("Wrote file: " + os.getcwd() + "/" + outputHandler.getFilename() + ". View the file or just run `buildid` again to see all the properties.");
 
 		if settings.info:
 			print outputHandler.toString() + "\n"
